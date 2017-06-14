c188c84aba6dbb7fc5f4cf7fc1be9e3985e8fd5f
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2017-06-14 15:03:20 +0200

    Adding fetching the binaries, libraries etc. Moving gem_amc_top.xml under ./xml. Cleaning after syn ensured

--- setup_ctp7.sh
+++ setup_ctp7.sh
@@ -105,6 +105,26 @@ then
     echo "Creating/updating CTP7 gemdaq directory structure"
     echo "ssh root@$ctp7host mkdir -p /mnt/persistent/gemdaq"
     ssh root@$ctp7host mkdir -p /mnt/persistent/gemdaq
+    echo "Fetching binaries, firmware and libraries"
+    echo "cd ./bin && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/ipbus && cd -"
+    cd ./bin && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/ipbus && cd -
+    echo "cd ./lib && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/liblog4cplus.so && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/libxhal_ctp7.so && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/libxerces-c.so && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/librwreg_ctp7.so && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/liblmdb.so && ln -sf librwreg_ctp7.so librwreg.so && ln -sf libxerces-c.so libxerces-c-3.1.so && cd -"
+    cd ./lib && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/liblog4cplus.so && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/libxhal_ctp7.so && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/libxerces-c.so && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/librwreg_ctp7.so && wget https://github.com/mexanick/xhal/releases/download/v2.0.0/liblmdb.so && ln -sf librwreg_ctp7.so librwreg.so && ln -sf libxerces-c.so libxerces-c-3.1.so && cd -
+    echo "cd ./python && wget https://github.com/mexanick/xhal/files/1071017/reg_interface.zip && unzip reg_interface.zip && rm reg_interface.zip && cd -"
+    cd ./python && wget https://github.com/mexanick/xhal/files/1071017/reg_interface.zip && unzip reg_interface.zip && rm reg_interface.zip && cd -
+    echo "cd ./fw && wget https://github.com/evka85/GEM_AMC/releases/download/v1.9.4/gem_ctp7_v1_9_4_GBT.bit && ln -sf gem_ctp7_v1_9_4_GBT.bit gem_ctp7.bit && cd -"
+    cd ./fw && wget https://github.com/evka85/GEM_AMC/releases/download/v1.9.4/gem_ctp7_v1_9_4_GBT.bit && ln -sf gem_ctp7_v1_9_4_GBT.bit gem_ctp7.bit && cd -
+    echo "cd ./oh_fw && wget https://github.com/thomaslenzi/OptoHybridv2/releases/download/2.2.D.FB/optohybrid_top.bit -O optohybrid_2.2.D.FB.bit && ln -sf optohybrid_2.2.D.FB.bit optohybrid_top.bit && wget https://github.com/thomaslenzi/OptoHybridv2/releases/download/2.2.D.FB/optohybrid_top.mcs -O optohybrid_2.2.D.FB.mcs && ln -sf optohybrid_2.2.D.FB.mcs optohybrid_top.mcs && cd -"
+    cd ./oh_fw && wget https://github.com/thomaslenzi/OptoHybridv2/releases/download/2.2.D.FB/optohybrid_top.bit -O optohybrid_2.2.D.FB.bit && ln -sf optohybrid_2.2.D.FB.bit optohybrid_top.bit && wget https://github.com/thomaslenzi/OptoHybridv2/releases/download/2.2.D.FB/optohybrid_top.mcs -O optohybrid_2.2.D.FB.mcs && ln -sf optohybrid_2.2.D.FB.mcs optohybrid_top.mcs && cd -
+    echo "cd ./xml && wget https://github.com/evka85/GEM_AMC/releases/download/v1.9.4/address_table_v1_9_4.zip && unzip address_table_v1_9_4.zip && cp address_table_v1_9_4/gem_amc_top.xml gem_amc_v1_9_4.xml && rm -rf address_table* && ln -sf gem_amc_v1_9_4.xml gem_amc_top.xml && cd -"
+    cd ./xml && wget https://github.com/evka85/GEM_AMC/releases/download/v1.9.4/address_table_v1_9_4.zip && unzip address_table_v1_9_4.zip && cp address_table_v1_9_4/gem_amc_top.xml gem_amc_v1_9_4.xml && rm -rf address_table* && ln -sf gem_amc_v1_9_4.xml gem_amc_top.xml && cd -
     echo "rsync -aXch --progress --partial --links bin fw lib oh_fw scripts xml python root@$ctp7host:/mnt/persistent/gemdaq/"
     rsync -aXch --progress --partial --links bin fw lib oh_fw scripts xml python root@$ctp7host:/mnt/persistent/gemdaq/
+    echo "Clearing folders"
+    rm ./bin/*.*
+    rm ./lib/*.*
+    rm ./fw/*.*
+    rm ./oh_fw/*.*
+    rm ./python/reg_interface/*.*
+    rm ./xml/*.*
 fi
